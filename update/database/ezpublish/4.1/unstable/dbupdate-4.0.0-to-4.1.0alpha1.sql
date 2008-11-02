b3f51f8a0ef10adc936f0784f6e5daa9ceec96bd
Gaetano Giunta <gg@ez.no>
2008-11-02 15:00:39 +0000

    - de-support Oracle 8
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@159 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- update/database/ezpublish/4.1/unstable/dbupdate-4.0.0-to-4.1.0alpha1.sql
+++ update/database/ezpublish/4.1/unstable/dbupdate-4.0.0-to-4.1.0alpha1.sql
@@ -22,3 +22,74 @@ ALTER TABLE ezurlalias_ml ADD alias_redirects number(11) default 1 NOT NULL;
 -- END: from 3.10.1
 
 ALTER TABLE ezbinaryfile MODIFY (mime_type VARCHAR2(255));
+
+CREATE TABLE ezcontentobject_state (
+    default_language_id integer DEFAULT 0 NOT NULL,
+    group_id integer DEFAULT 0 NOT NULL,
+    id integer NOT NULL,
+    identifier varchar2(45) DEFAULT '' NOT NULL,
+    language_mask integer DEFAULT 0 NOT NULL,
+    priority integer DEFAULT 0 NOT NULL
+);
+
+CREATE TABLE ezcontentobject_state_group (
+    default_language_id integer DEFAULT 0 NOT NULL,
+    id integer NOT NULL,
+    identifier varchar2(45) DEFAULT '' NOT NULL,
+    language_mask integer DEFAULT 0 NOT NULL
+);
+
+CREATE TABLE ezcontentobject_state_group_language (
+    contentobject_state_group_id integer DEFAULT 0 NOT NULL,
+    description text NOT NULL,
+    language_id integer DEFAULT 0 NOT NULL,
+    name varchar2(45) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE ezcontentobject_state_language (
+    contentobject_state_id integer DEFAULT 0 NOT NULL,
+    "default" integer,
+    description clob NOT NULL,
+    language_id integer DEFAULT 0 NOT NULL,
+    name varchar2(45) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE ezcontentobject_state_link (
+    contentobject_id integer DEFAULT 0 NOT NULL,
+    contentobject_state_id integer DEFAULT 0 NOT NULL
+);
+
+CREATE UNIQUE INDEX ezcontentobject_state_identifier ON ezcontentobject_state (group_id, identifier);
+
+CREATE UNIQUE INDEX ezcontentobject_state_group_identifier ON ezcontentobject_state_group (identifier);
+
+ALTER TABLE ezcontentobject_state
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE ezcontentobject_state_group
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE ezcontentobject_state_group_language
+    ADD PRIMARY KEY (language_id, contentobject_state_group_id);
+
+ALTER TABLE ezcontentobject_state_language
+    ADD PRIMARY KEY (contentobject_state_id, language_id);
+
+ALTER TABLE ezcontentobject_state_link
+    ADD PRIMARY KEY (contentobject_id, contentobject_state_id);
+
+CREATE SEQUENCE s_ezcontentobject_state;
+CREATE OR REPLACE TRIGGER ezcontentobject_state_tr
+BEFORE INSERT ON ezurlwildcard FOR EACH ROW WHEN (new.id IS NULL)
+BEGIN
+  SELECT s_ezcontentobject_state.nextval INTO :new.id FROM dual;
+END;
+/
+
+CREATE SEQUENCE s_ezcontentobject_state_group;
+CREATE OR REPLACE TRIGGER ezcontentobject_state_group_tr
+BEFORE INSERT ON ezcontentobject_state_group FOR EACH ROW WHEN (new.id IS NULL)
+BEGIN
+  SELECT s_ezcontentobject_state_group.nextval INTO :new.id FROM dual;
+END;
+/
