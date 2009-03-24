5c20da926f4f33c01690b5074b0bfb06672e76b2
Gaetano Giunta <gg@ez.no>
2009-03-24 23:15:50 +0000

    - embryo of test suite
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@247 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- extension/ezoracle/tests/suite.php
+++ extension/ezoracle/tests/suite.php
@@ -5,6 +5,8 @@
  * @copyright Copyright (C) 2009 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
  * @package tests
+ *
+ * @todo add a suite of tests for cluster mode
  */
 
 class ezoracleTestSuite extends ezpTestSuite
@@ -14,7 +16,7 @@ class ezoracleTestSuite extends ezpTestSuite
         parent::__construct();
         $this->setName( "eZ Oracle Extension Test Suite" );
 
-        /// @todo...
+        $this->addTestSuite( 'eZPostgreSQLDBTest' );
     }
 
     public static function suite()
