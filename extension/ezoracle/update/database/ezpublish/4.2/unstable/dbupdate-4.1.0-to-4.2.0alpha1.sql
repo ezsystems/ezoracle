d5704180f42767b3af49a309eda8d6a36387c3d2
Gaetano Giunta <gg@ez.no>
2009-04-16 17:18:56 +0000

    - merge changes to 4.2 update script; add proper skip notices
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@258 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- extension/ezoracle/update/database/ezpublish/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
+++ extension/ezoracle/update/database/ezpublish/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
@@ -1,5 +1,13 @@
 UPDATE ezsite_data SET value='4.2.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
+-- START: from 4.1.1
+ALTER TABLE ezworkflow_event MODIFY (
+    data_text1 VARCHAR2(255),
+    data_text2 VARCHAR2(255),
+    data_text3 VARCHAR2(255),
+    data_text4 VARCHAR2(255));
+-- END: from 4.1.1
+
 -- START: from 4.1.0
 -- END: from 4.1.0
