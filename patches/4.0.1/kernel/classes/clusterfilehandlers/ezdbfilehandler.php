95d7f2f905e33f4df04cf2c7820d1199189a1060
Gaetano Giunta <gg@ez.no>
2009-04-16 21:16:49 +0000

    - update copyright info to 2009
    git-svn-id: http://svn.projects.ez.no/ezoracle/branches/1.8@261 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- patches/4.0.1/kernel/classes/clusterfilehandlers/ezdbfilehandler.php
+++ patches/4.0.1/kernel/classes/clusterfilehandlers/ezdbfilehandler.php
@@ -7,7 +7,7 @@
 // SOFTWARE NAME: eZ Oracle
 // SOFTWARE RELEASE: 1.8.x
 // BUILD VERSION: 21995
-// COPYRIGHT NOTICE: Copyright (C) 1999-2008 eZ Systems AS
+// COPYRIGHT NOTICE: Copyright (C) 1999-2009 eZ Systems AS
 // SOFTWARE LICENSE: GNU General Public License v2.0
 // NOTICE: >
 //   This program is free software; you can redistribute it and/or
