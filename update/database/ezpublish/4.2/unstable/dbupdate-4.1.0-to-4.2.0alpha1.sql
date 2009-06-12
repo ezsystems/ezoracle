cddd8cb856dea947a34e68e40848cff6d869bdda
Gaetano Giunta <gg@ez.no>
2009-06-12 14:40:18 +0000

    - merge changes to update sql
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@274 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- update/database/ezpublish/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
+++ update/database/ezpublish/4.2/unstable/dbupdate-4.1.0-to-4.2.0alpha1.sql
@@ -1,6 +1,14 @@
 UPDATE ezsite_data SET value='4.2.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
+-- START: from 4.1.4
+CREATE INDEX ezkeyword_attr_link_oaid ON ezkeyword_attribute_link (objectattribute_id);
+-- END: from 4.1.4
+
+-- START: from 4.1.2
+ALTER TABLE ezsession MODIFY (user_hash NULL);
+-- END: from 4.1.2
+
 -- START: from 4.1.1
 ALTER TABLE ezworkflow_event MODIFY (
     data_text1 VARCHAR2(255),
