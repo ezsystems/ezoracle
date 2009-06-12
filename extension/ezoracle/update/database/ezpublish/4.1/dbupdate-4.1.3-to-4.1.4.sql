76a3efe06544b7399c8b0d1aa1adde4f3a5ca523
Gaetano Giunta <gg@ez.no>
2009-06-12 14:34:52 +0000

    - add sql update for 4.0.6, 4.0.7 (merge from stable/1.8 rev. 272)
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@273 436571c7-bdc5-44d1-b1ef-e55a79f94445

---
+++ extension/ezoracle/update/database/ezpublish/4.1/dbupdate-4.1.3-to-4.1.4.sql
@@ -0,0 +1,4 @@
+UPDATE ezsite_data SET value='4.1.4' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+CREATE INDEX ezkeyword_attr_link_oaid ON ezkeyword_attribute_link ( objectattribute_id);
