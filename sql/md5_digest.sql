99e3bba3298d122667df3ebb78644e850eb8bdd7
Gaetano Giunta <gg@ez.no>
2009-07-27 12:30:18 +0000

    - Improvement for bug #013469: allowed the md5_digest function to return a value for empty strings
    git-svn-id: http://svn.projects.ez.no/ezoracle/branches/1.8@290 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- sql/md5_digest.sql
+++ sql/md5_digest.sql
@@ -4,8 +4,10 @@ RETURN VARCHAR2 IS
 -- Return an MD5 hash of the input string.
 --
 BEGIN
-RETURN
-    lower(dbms_obfuscation_toolkit.md5(input =>
-                                       utl_raw.cast_to_raw(vin_string)));
+    IF vin_string IS NULL THEN
+        RETURN 'd41d8cd98f00b204e9800998ecf8427e';
+    ELSE
+        RETURN lower(dbms_obfuscation_toolkit.md5(input =>utl_raw.cast_to_raw(vin_string)));
+    END IF;
 END md5_digest;
 /
