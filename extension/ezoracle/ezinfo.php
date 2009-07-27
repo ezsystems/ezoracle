dafb4a12410b635e1b86d78a14a09741d6ede839
Gaetano Giunta <gg@ez.no>
2009-07-27 12:31:18 +0000

    - Improvement for bug #013469: allowed the md5_digest function to return a value for empty strings (merge from stable/1.8.3 rev. 290)
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@291 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- extension/ezoracle/ezinfo.php
+++ extension/ezoracle/ezinfo.php
@@ -29,7 +29,7 @@ class ezoracleInfo
     static function info()
     {
         return array( 'Name' => "eZ Oracle",
-                      'Version' => "2.0.1",
+                      'Version' => "2.0.2",
                       'Copyright' => "Copyright (C) 1999-2009 eZ Systems AS",
                       'License' => "GNU General Public License v2.0"
                      );
