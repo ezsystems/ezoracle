715acf60e73c5b9065b65e92ab10d1b8add74d45
Gaetano Giunta <gg@ez.no>
2009-09-07 13:12:31 +0000

    - add releases 1.8.1 and 1.8.2 to the 'releases" branch for historic sake
    git-svn-id: http://svn.projects.ez.no/ezoracle/releases/1.8.2@295 436571c7-bdc5-44d1-b1ef-e55a79f94445

---
+++ settings/file.ini.append.php
@@ -0,0 +1,4 @@
+<?php /*
+[ClusteringSettings]
+ExtensionDirectories[]=ezoracle
+*/ ?>
---
+++ update/database/ezpublish/3.9/dbupdate-3.9.3-to-3.9.4.sql
@@ -0,0 +1,8 @@
+UPDATE ezsite_data SET value='3.9.4' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='10' WHERE name='ezpublish-release';
+
+CREATE INDEX ezsearch_word_obj_count ON ezsearch_word(object_count);
+
+DROP INDEX ezurl_url;
+ALTER TABLE ezurl ALTER url TYPE varchar2(2000);
+CREATE INDEX ezurl_url ON ezurl(url);
