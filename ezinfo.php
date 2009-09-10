9e2228de187b23cc9e8e82666e1116ac43802619
Gaetano Giunta <gg@ez.no>
2009-09-10 16:00:05 +0000

    - Fix bug #015462: The db upgrade scripts for oracle do not work on table ezpending_actions
    git-svn-id: http://svn.projects.ez.no/ezoracle/branches/1.6@302 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- ezinfo.php
+++ ezinfo.php
@@ -29,7 +29,7 @@ class ezoracleInfo
     function info()
     {
         return array( 'Name' => "eZ Oracle",
-                      'Version' => "1.6",
+                      'Version' => "1.6.1",
                       'Copyright' => "Copyright (C) 1999-2008 eZ Systems AS",
                       'License' => "GNU General Public License v2.0"
                      );
