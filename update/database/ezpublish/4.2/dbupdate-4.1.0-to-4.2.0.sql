49415b36c0d8868d84038f3380c0028a2ca71193
Gaetano Giunta <gg@ez.no>
2009-09-30 09:32:08 +0000

    - add update sql needed for 4.2.0 final (merge from branches/2.0 the files forgotten in previous export)
    git-svn-id: http://svn.projects.ez.no/ezoracle/releases/2.0.2@320 436571c7-bdc5-44d1-b1ef-e55a79f94445

---
+++ update/database/ezpublish/4.2/dbupdate-4.1.0-to-4.2.0.sql
@@ -0,0 +1,26 @@
+UPDATE ezsite_data SET value='4.2.0' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+-- START: from 4.1.0
+-- END: from 4.1.0
+
+-- START: from 4.1.1
+ALTER TABLE ezworkflow_event MODIFY (
+    data_text1 VARCHAR2(255),
+    data_text2 VARCHAR2(255),
+    data_text3 VARCHAR2(255),
+    data_text4 VARCHAR2(255));
+-- END: from 4.1.1
+
+-- START: from 4.1.2
+ALTER TABLE ezsession MODIFY (user_hash NULL);
+-- END: from 4.1.2
+
+-- START: from 4.1.4
+CREATE INDEX ezkeyword_attr_link_oaid ON ezkeyword_attribute_link ( objectattribute_id );
+CREATE INDEX ezinfocollection_co_id_created ON ezinfocollection( contentobject_id, created );
+-- END: from 4.1.4
+
+CREATE INDEX policy_id ON ezpolicy_limitation ( policy_id );
+CREATE INDEX wid_version_placement ON ezworkflow_event ( workflow_id, version, placement );
+CREATE INDEX hash_key ON ezuser_accountkey ( hash_key );
