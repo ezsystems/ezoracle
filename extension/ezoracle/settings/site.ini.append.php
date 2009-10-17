116196898a425e0d3869e55e3ae5ce2ef1312cc2
Gaetano Giunta <gg@ez.no>
2009-10-17 22:31:07 +0000

    - added example configs to ini files
    git-svn-id: http://svn.projects.ez.no/ezoracle/branches/2.0@334 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- extension/ezoracle/settings/site.ini.append.php
+++ extension/ezoracle/settings/site.ini.append.php
@@ -2,4 +2,14 @@
 [DatabaseSettings]
 ImplementationAlias[oracle]=eZOracleDB
 ImplementationAlias[ezoracle]=eZOracleDB
+
+# Example configuration for connecting to an oracle db
+#DatabaseImplementation=ezoracle
+#Server=
+#Port=
+#User=scott
+#Password=tiger
+#Database=orcl
+#Charset=
+
 */ ?>
