d5fefa32b39ad8dd76b88379b0c63759b82e79ce
Gaetano Giunta <gg@ez.no>
2010-02-25 17:13:04 +0000

    - add patches for all 4.0, 4.1 and 4.2 release that facilitate the system upgrade db check
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@391 436571c7-bdc5-44d1-b1ef-e55a79f94445

---
+++ extension/ezoracle/patches/4.1.2/kernel/steup/systemupgrade.php
@@ -0,0 +1,89 @@
+<?php
+//
+// Created on: <04-Feb-2004 21:56:50 kk>
+//
+// SOFTWARE NAME: eZ Publish
+// SOFTWARE RELEASE: 4.1.2
+// BUILD VERSION: 23601
+// COPYRIGHT NOTICE: Copyright (C) 1999-2009 eZ Systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+
+$Module = $Params['Module'];
+
+require_once( "kernel/common/template.php" );
+define( 'MD5_SUM_LIST_FILE', 'share/filelist.md5' );
+
+$tpl = templateInit();
+
+$tpl->setVariable( 'md5_result', false );
+$tpl->setVariable( 'upgrade_sql', false );
+
+if ( $Module->isCurrentAction( 'MD5Check' ) )
+{
+    if ( !file_exists( MD5_SUM_LIST_FILE ) )
+    {
+        $tpl->setVariable( 'md5_result', 'failed' );
+        $tpl->setVariable( 'failure_reason',
+                           ezi18n( 'kernel/setup', 'File %1 does not exist. '.
+                                    'You should copy it from the recent eZ Publish distribution.',
+                                    null, array( MD5_SUM_LIST_FILE ) ) );
+    }
+    else
+    {
+        $checkResult = eZMD5::checkMD5Sums( 'share/filelist.md5' );
+
+        if ( count( $checkResult ) == 0 )
+        {
+            $tpl->setVariable( 'md5_result', 'ok' );
+        }
+        else
+        {
+            $tpl->setVariable( 'md5_result', $checkResult );
+        }
+    }
+}
+
+if ( $Module->isCurrentAction( 'DBCheck' ) )
+{
+    $db = eZDB::instance();
+    $dbSchema = eZDbSchema::instance();
+    // read original schema from dba file
+    $originalSchema = eZDbSchema::read( 'share/db_schema.dba' );
+    // transform schema to 'localized' version for current db
+    // (we might as well convert $dbSchema to generic format and diff in generic format,
+    // but eZDbSchemaChecker::diff does not know how to re-localize the generated sql
+    $dbSchema->transformSchema( $originalSchema, true );
+    $differences = eZDbSchemaChecker::diff( $dbSchema->schema( array( 'format' => 'local' ) ), $originalSchema );
+    $sqlDiff = $dbSchema->generateUpgradeFile( $differences );
+
+    if ( strlen( $sqlDiff ) == 0 )
+    {
+        $tpl->setVariable( 'upgrade_sql', 'ok' );
+    }
+    else
+    {
+        $tpl->setVariable( 'upgrade_sql', $sqlDiff );
+    }
+}
+
+$Result = array();
+$Result['content'] = $tpl->fetch( "design:setup/systemupgrade.tpl" );
+$Result['path'] = array( array( 'url' => false,
+                                'text' => ezi18n( 'kernel/setup', 'System Upgrade' ) ) );
+?>
