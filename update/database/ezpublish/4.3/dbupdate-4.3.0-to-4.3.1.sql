acb419f9ab9f00147fa820bef06938c1d499e928
Gaetano Giunta <gg@ez.no>
2010-08-29 13:14:31 +0000

    - add sql update scripts for (unlereased) 4.3.1 ezp version; split off cluster update scripts for previous revisions in separate files
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@435 436571c7-bdc5-44d1-b1ef-e55a79f94445

---
+++ update/database/ezpublish/4.3/dbupdate-4.3.0-to-4.3.1.sql
@@ -0,0 +1,4 @@
+UPDATE ezsite_data SET value='4.3.1' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+UPDATE ezcontentclass_attribute SET can_translate=0 WHERE data_type_string='ezuser';
