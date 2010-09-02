39f24a96d0bbef621678e7a82ba70ed5140b4e82
Gaetano Giunta <gg@ez.no>
2010-09-02 10:03:56 +0000

    - remove update scripts for 4.4: the 2.1 ezoracle branch is for that
    git-svn-id: http://svn.projects.ez.no/ezoracle/stable/2.0@484 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- update/database/ezpublish/4.4/unstable/dbupdate-4.4.0alpha5-to-4.4.0beta1.sql
+++
@@ -1,4 +0,0 @@
-UPDATE ezsite_data SET value='4.4.0beta1' WHERE name='ezpublish-version';
-UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
-
-ALTER TABLE ezsection RENAME COLUMN section_identifier TO identifier;
