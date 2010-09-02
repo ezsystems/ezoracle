319dd4913402b31618142bffddab5adeb1aaa94b
Gaetano Giunta <gg@ez.no>
2010-09-02 12:17:57 +0000

    - doc updates
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@490 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- extension/ezoracle/ezinfo.php
+++ extension/ezoracle/ezinfo.php
@@ -29,7 +29,7 @@ class ezoracleInfo
     static function info()
     {
         return array( 'Name' => "eZ Oracle",
-                      'Version' => '2.0.4-dev',
+                      'Version' => '2.1.0-dev',
                       'Copyright' => "Copyright (C) 1999-2010 eZ Systems AS",
                       'License' => "GNU General Public License v2.0"
                      );
