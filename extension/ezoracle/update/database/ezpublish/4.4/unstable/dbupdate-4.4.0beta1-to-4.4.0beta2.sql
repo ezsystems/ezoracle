05f4809d6b38e33011626f204834baaa33c785df
Xiongjie Chen <xc@ez.no>
2010-09-23 12:03:43 +0000

    Corrected update script error.
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@514 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- extension/ezoracle/update/database/ezpublish/4.4/unstable/dbupdate-4.4.0beta1-to-4.4.0beta2.sql
+++ extension/ezoracle/update/database/ezpublish/4.4/unstable/dbupdate-4.4.0beta1-to-4.4.0beta2.sql
@@ -1,7 +1,12 @@
 UPDATE ezsite_data SET value='4.4.0beta2' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
-ALTER TABLE ezpreferences MODIFY ( value LONG );
+
+ALTER TABLE ezpreferences RENAME COLUMN value TO value_temp;
+ALTER TABLE ezpreferences ADD value LONG NULL;
+UPDATE ezpreferences set value=value_temp;
+ALTER TABLE ezpreferences DROP COLUMN value_temp;
+
 ALTER TABLE ezpolicy ADD original_id INTEGER DEFAULT 0 NOT NULL;
 CREATE INDEX ezpolicy_original_id ON ezpolicy( original_id );
 
