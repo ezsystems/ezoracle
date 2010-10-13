10fe3ce95d6e5c393bc1a9e0389120d365377927
Gaetano Giunta <gg@ez.no>
2010-10-13 11:32:16 +0000

    - Fixed bug #017451: ezoracle: Cannot add a comment in Collaboration Approval screen
    git-svn-id: http://svn.projects.ez.no/ezoracle/stable/2.0@527 436571c7-bdc5-44d1-b1ef-e55a79f94445

---
+++ update/database/ezoracle/2.0/dbupdate-2.0.4-2.0.5.sql
@@ -0,0 +1,3 @@
+ALTER TABLE ezcollab_simple_message MODIFY ( data_text1 NULL );
+ALTER TABLE ezcollab_simple_message MODIFY ( data_text2 NULL );
+ALTER TABLE ezcollab_simple_message MODIFY ( data_text3 NULL );
