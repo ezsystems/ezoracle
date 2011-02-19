0b70b7c2c2b1a17a306c0f0af081b75dd7c167f4
Bertrand Dunogier <bd@ez.no>
2011-02-19 16:41:19 +0000

    Updated ezinfo.php version number & license tag
    git-svn-id: http://svn.projects.ez.no/ezoracle/stable/2.1@561 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- ezinfo.php
+++ ezinfo.php
@@ -29,9 +29,9 @@ class ezoracleInfo
     static function info()
     {
         return array( 'Name' => "eZ Oracle",
-                      'Version' => '2.1.1-dev',
+                      'Version' => '//autogentag//',
                       'Copyright' => "Copyright (C) 1999-2010 eZ Systems AS",
-                      'License' => "GNU General Public License v2.0"
+                      'License' => "//EZP_LICENSE//"
                      );
     }
 }
