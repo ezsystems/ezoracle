e655caee7fb92efc643ca0cc64d9e112779b6ec5
Gaetano Giunta <gg@ez.no>
2011-02-23 16:50:49 +0000

    - merge changes from mysql/postgres updgrade scripts: new table definition
    git-svn-id: http://svn.projects.ez.no/ezoracle/trunk@562 436571c7-bdc5-44d1-b1ef-e55a79f94445

--- update/database/ezpublish/4.5/unstable/dbupdate-4.4.0-to-4.5.0alpha1.sql
+++ update/database/ezpublish/4.5/unstable/dbupdate-4.4.0-to-4.5.0alpha1.sql
@@ -1,2 +1,12 @@
 UPDATE ezsite_data SET value='4.5.0alpha1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+
+CREATE TABLE ezpublishingqueueprocesses (
+    created INTEGER,
+    ezcontentobject_version_id INTEGER DEFAULT 0 NOT NULL,
+    finished INTEGER,
+    pid INTEGER,
+    started INTEGER,
+    status INTEGER,
+	PRIMARY KEY( ezcontentobject_version_id )
+);
