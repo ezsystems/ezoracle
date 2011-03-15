3f038984adcabb94c1dfcadf3335ef7e299a77dd
xc <xc@ez.no>
2011-03-15 17:34:14 +0100

    Fixed #018137: ORACLE : ezp4.4->ezp4.5 upgrade : Database check fails after upgrade

--- update/database/ezflow/2.3.0/dbupdate-2.2.0-to-2.3.0.sql
+++ update/database/ezflow/2.3.0/dbupdate-2.2.0-to-2.3.0.sql
@@ -2,7 +2,7 @@ BEGIN
    DECLARE
       INDEX_NAME  VARCHAR2(50)  DEFAULT '';
    BEGIN
-      select a.index_name into INDEX_NAME from user_ind_columns a, user_ind_columns b, user_ind_columns c where a.index_name=b.index_name and b.index_name=c.index_name and a.column_name='BLOCK_ID' and a.column_position=1 and b.column_name='TS_PUBLICATION' and b.column_position=2 and c.column_name='PRIORITY' and c.column_position=3;
+      select a.index_name into INDEX_NAME from user_ind_columns a, user_ind_columns b, user_ind_columns c where a.index_name=b.index_name and b.index_name=c.index_name and a.table_name='EZM_POOL' and a.column_name='BLOCK_ID' and a.column_position=1 and b.column_name='TS_PUBLICATION' and b.column_position=2 and c.column_name='PRIORITY' and c.column_position=3;
       BEGIN
         EXECUTE IMMEDIATE 'DROP INDEX '|| INDEX_NAME;
       END;
