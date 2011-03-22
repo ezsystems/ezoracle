5997f1e3e0e073ce46a6644cf6fe072f37ec58d1
Gaetano Giunta <gg@ez.no>
2011-03-22 17:24:48 +0100

    Delete update script 2.1.0-to-2.1.1, since 2.2.0 branches off from 2.1.0

--- update/database/ezoracle/2.1/dbupdate-2.1.0-2.1.1.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ezcollab_simple_message MODIFY ( data_text1 NULL );
-ALTER TABLE ezcollab_simple_message MODIFY ( data_text2 NULL );
-ALTER TABLE ezcollab_simple_message MODIFY ( data_text3 NULL );
