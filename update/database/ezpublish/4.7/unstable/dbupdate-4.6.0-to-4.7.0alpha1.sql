d05fb2ce18a274918f4bed179a55ebf103c597a1
Damien Pobel <dp@ez.no>
2012-02-14 14:19:09 +0100

    Fixed: sync dbupdate script to fix #18886

--- update/database/ezpublish/4.7/unstable/dbupdate-4.6.0-to-4.7.0alpha1.sql
+++ update/database/ezpublish/4.7/unstable/dbupdate-4.6.0-to-4.7.0alpha1.sql
@@ -13,3 +13,10 @@ END;
 
 
 ALTER TABLE ezpending_actions ADD CONSTRAINT pk_ezpending_actions PRIMARY KEY (id);
+
+
+-- Cleanup for #18886 
+-- when a user is manually enabled through the admin interface, 
+-- the corresponding ezuser_accountkey record is not removed 
+DELETE FROM ezuser_accountkey WHERE user_id IN ( SELECT user_id FROM ezuser_setting WHERE is_enabled = 1 );
+
