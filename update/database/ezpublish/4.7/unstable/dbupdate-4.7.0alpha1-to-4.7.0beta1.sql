945f301b7930f1747fbe7a641d021d7dc717b01c
Patrick Allaert <pa@ez.no>
2012-03-28 17:15:11 +0200

    Fixed #019236: decimals of prices are incorrectly stored (float -> double) part2

--- update/database/ezpublish/4.7/unstable/dbupdate-4.7.0alpha1-to-4.7.0beta1.sql
+++ update/database/ezpublish/4.7/unstable/dbupdate-4.7.0alpha1-to-4.7.0beta1.sql
@@ -5,3 +5,14 @@ ALTER TABLE ezcontentobject_attribute ADD ( data_float_tmp BINARY_DOUBLE DEFAULT
 UPDATE ezcontentobject_attribute SET data_float_tmp = data_float;
 ALTER TABLE ezcontentobject_attribute DROP ( data_float );
 ALTER TABLE ezcontentobject_attribute RENAME COLUMN data_float_tmp TO data_float;
+
+ALTER TABLE ezcontentclass_attribute ADD ( data_float1_tmp BINARY_DOUBLE DEFAULT 0 NULL );
+ALTER TABLE ezcontentclass_attribute ADD ( data_float2_tmp BINARY_DOUBLE DEFAULT 0 NULL );
+ALTER TABLE ezcontentclass_attribute ADD ( data_float3_tmp BINARY_DOUBLE DEFAULT 0 NULL );
+ALTER TABLE ezcontentclass_attribute ADD ( data_float4_tmp BINARY_DOUBLE DEFAULT 0 NULL );
+UPDATE ezcontentclass_attribute SET data_float1_tmp = data_float1, data_float2_tmp = data_float2, data_float3_tmp = data_float3, data_float4_tmp = data_float4;
+ALTER TABLE ezcontentclass_attribute DROP ( data_float1, data_float2, data_float3, data_float4 );
+ALTER TABLE ezcontentclass_attribute RENAME COLUMN data_float1_tmp TO data_float1;
+ALTER TABLE ezcontentclass_attribute RENAME COLUMN data_float2_tmp TO data_float2;
+ALTER TABLE ezcontentclass_attribute RENAME COLUMN data_float3_tmp TO data_float3;
+ALTER TABLE ezcontentclass_attribute RENAME COLUMN data_float4_tmp TO data_float4;
