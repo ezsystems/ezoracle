8849aee21b44c6fb2e0bab6ffb558b4d7172f096
Petar Spanja <petar.spanja@gmail.com>
2013-01-09 12:02:40 +0100

    Added: cleanup queries for ezcontentobject_link table

--- update/database/ezpublish/5.1/dbupdate-5.0.0-to-5.1.0.sql
+++ update/database/ezpublish/5.1/dbupdate-5.0.0-to-5.1.0.sql
@@ -6,3 +6,6 @@ CREATE INDEX ezcontentobject_tree_remote_id ON ezcontentobject_tree (remote_id);
 CREATE INDEX ezcontobj_version_obj_status ON ezcontentobject_version (contentobject_id, status);
 CREATE INDEX ezpolicy_role_id ON ezpolicy (role_id);
 CREATE INDEX ezpolicy_limit_value_limit_id ON ezpolicy_limitation_value (limitation_id);
+
+DELETE FROM ezcontentobject_link WHERE op_code <> 0;
+DELETE FROM ezcontentobject_link WHERE relation_type = 0;
